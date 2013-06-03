87ff8e22e8e0b8bdd29a04c065b7e0ffa990e722
Aravind Alwan <aravindalwan@gmail.com>
2013-06-03 03:34:30 -0500

    Add safety check to allow program to save results even if postprocessor fails
    Changelog:
    
    - Catch exception when running post-processor function in ParametricSolverMaster
    - Log warning in main logger and ignore error
    
    With this change, the code will run to completion even if the post-processor
    fails. This is to prevent loss of data in case the post-processor has not been
    properly defined.

--- unyquesolver/psolver.py
+++ unyquesolver/psolver.py
@@ -217,8 +217,13 @@ class ParametricSolverMaster(ParametricSolver):
                              'tag': tag}
                 logmessage = 'Replicate: %(replicate)3d     Tag: %(tag)s'
                 if postprocessor:
-                    meta_data['result'] = postprocessor(result)
-                    logmessage = logmessage + '     Result: %(result)1.6f'
+                    try:
+                        meta_data['result'] = postprocessor(result)
+                        logmessage = logmessage + '     Result: %(result)1.6f'
+                    except StandardError as e: # Error in postproc function
+                        # Log warning and ignore error
+                        self._log.warning('Error in post-processing function: '
+                                          + e.strerror)
                 self._results_log.log(logmanager.REPLICATE,
                                       logmessage, meta_data, extra = meta_data)
 
